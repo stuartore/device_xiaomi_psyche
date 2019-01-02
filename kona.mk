0419965ba1e335fc24aaa40c02c4f54e53c82954
Harshit Jain <harshitjain6751@gmail.com>
2019-01-02 15:11:56 +0530

    sm8250-common: Strip off some debug packages
    * Ref: https://source.android.com/devices/tech/dalvik/configure#compilation_options
    
    Q: PRODUCT_MINIMIZE_JAVA_DEBUG_INFO wth does this do?
    A: https://android.googlesource.com/platform/build/+/master/target/product/go_defaults_common.mk#68

--- kona.mk
+++ kona.mk
@@ -99,6 +99,11 @@ endif
 PRODUCT_PACKAGES += \
     com.dsi.ant@1.0.vendor
 
+# ART Debugging (Disable)
+USE_DEX2OAT_DEBUG := false
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
