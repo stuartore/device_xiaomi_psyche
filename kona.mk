54ba8dce6b6a825a9572485409504858f19c9d08
johnmart19 <johnivan19999@gmail.com>
2023-05-31 20:22:41 +0300

    sm8250-common: Import Xiaomi Misys Hal
    - Import Xiaomi Misys hal for camera processing
    - Build Legacy android.hidl.memory.block, android.hidl.manager in vendor for misys serivce
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.xiaomi.hardware.misys@3.0-service": library "android.hidl.memory.block@1.0.so" not found: needed by /vendor/lib64/vendor.xiaomi.hardware.misys@3.0.so in namespace (default)
    
    Co-authored-by: klozz <klozz@thexperienceproject.org>

--- kona.mk
+++ kona.mk
@@ -191,6 +191,14 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+# Camera Legacy HIDL deps
+PRODUCT_PACKAGES += \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0.vendor \
+    android.hidl.memory.block@1.0 \
+    android.hidl.memory.block@1.0.vendor \
+    android.hidl.memory@1.0-impl
+
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
