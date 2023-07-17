ef97795d1f6f24c8a5e7985a1a6d1ba6f63e821a
stuartore <stuartore@foxmail.com>
2023-07-17 23:27:42 +0800

    psyche: hardware: use xiaomi mars devs

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -169,7 +169,7 @@ git_check_dir(){
 psyche_deps(){
 	# use git_check_dir to setup dependencies
 
-	git_check_dir https://github.com/Evolution-X/hardware_xiaomi tiramisu hardware/xiaomi
+	git_check_dir https://github.com/xiaomi-mars-devs/android_hardware_xiaomi lineage-20-Gerrit hardware/xiaomi
 
 	git_check_dir https://github.com/stuartore/device_xiaomi_psyche $1 device/xiaomi/psyche
 	git_check_dir https://gitlab.com/stuartore/android_vendor_xiaomi_psyche $2 vendor/xiaomi/psyche
