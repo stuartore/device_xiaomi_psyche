9b6efd468c511680eb4ed798cdbaaca5e9a891b7
stuartore <stuartore@foxmail.com>
2023-07-18 11:26:51 +0800

    Revert "psyche: pocket mode: add"
    This reverts commit b7eecbc132d67603fa69a4286986816fbe84b621.

--- device.mk
+++ device.mk
@@ -520,12 +520,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
-# PocketMode
-PRODUCT_PACKAGES += \
-    PocketMode
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/pocket/privapp-permissions-pocketmode.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-pocketmode.xml
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
