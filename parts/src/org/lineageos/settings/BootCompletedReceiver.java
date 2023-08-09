8e12f9c8d2343c80a16ed1ff6e79d358a704cb0d
stuartore <stuartore@foxmail.com>
2023-08-09 13:43:26 +0800

    Revert "psyche: parts: fix compile error"
    This reverts commit 30e67b2b857ab3abb9da07ed10a005705dc30a79.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -68,6 +68,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
         RefreshUtils.startService(context);
+        FileUtils.enableService(context);
 
        //Micro-Service to restore sata of dt2w on reboot
        SharedPreferences prefs = context.getSharedPreferences(SHAREDD2TW, Context.MODE_PRIVATE);
