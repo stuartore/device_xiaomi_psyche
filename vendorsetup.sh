a1700fd0d931460867e7ab0213dc0c2182f0d665
stuartore <stuart.dr@outlook.com>
2024-10-25 22:39:32 +0800

    psyche: vendorsetup: correct repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -249,7 +249,7 @@ source_xiaomi_psyche-devs(){
 	git_check_dir https://github.com/xiaomi-psyche-development/android_device_xiaomi_psyche lineage-21 device/xiaomi/psyche
  	git_check_dir https://github.com/xiaomi-psyche-development/android_device_xiaomi_sm8250-common lineage-21 device/xiaomi/sm8250-common
  	git_check_dir https://github.com/xiaomi-psyche-development/proprietary_vendor_xiaomi_psyche lineage-21 vendor/xiaomi/psyche
-  	git_check_dir https://github.com/xiaomi-psyche-development/android_device_xiaomi_sm8250-common lineage-21 vendor/xiaomi/sm8250-common
+  	git_check_dir https://github.com/xiaomi-psyche-development/proprietary_vendor_xiaomi_sm8250-common lineage-21 vendor/xiaomi/sm8250-common
    	git_check_dir https://github.com/Rocky7842/android_kernel_xiaomi_sm8250 lineage-21 kernel/xiaomi/sm8250
 }
 
